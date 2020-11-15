5347cca9026a93c7480d8cd015f9849bc284eee6
Grzegorz Swiecinski <grzegorz.swiecinski@gmail.com>
2020-11-15 23:34:14 +0100

    Po dodaniu Usera

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Test2", password: "test2")
+User.create(username: "Test3", password: "test3")
