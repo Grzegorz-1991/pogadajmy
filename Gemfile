f2c0d032bc7ce7661f6afb6f96692ed96539f2fa
Grzegorz Swiecinski <grzegorz.swiecinski@gmail.com>
2020-11-18 22:43:47 +0100

    gotowa apka

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'semantic-ui-sass'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'hirb'
+gem 'redis'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,33 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new,:create]
+
     def new
 
+    end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "Poprawnie zalogowano"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Nieprawidłowe hasło lub login"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "Zostałeś poprawnie wylogowany"
+        redirect_to login_path
+    end
+
+    private
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "Jesteś już zalogowany!"
+            redirect_to root_path
+        end
     end
 end
